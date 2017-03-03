1d4ff1a076278e4efb86273e55cf9f9015a26a94
Justin Kizer <justinkizer@users.noreply.github.com>
2017-03-03 09:40:58 -0800

    Initial commit for React Components exercises

---
+++ W6D5/Calculator/calculator.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+class Calculator extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {num1: "", num2: "", result: 0};
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
+    this.clear = this.clear.bind(this);
+  }
+
+  setNum1(e) {
+    const num1 = (e.target.value ? parseInt(e.target.value) : "");
+    this.setState({num1});
+  }
+
+  setNum2(e) {
+    const num2 = (e.target.value ? parseInt(e.target.value) : "");
+    this.setState({num2});
+  }
+
+  add(e) {
+    e.preventDefault();
+    const num1 = this.state.num1;
+    const num2 = this.state.num2;
+    const result = ((num1 === "" && num2 === "") ? 0 : num1 + num2);
+    this.setState({result});
+  }
+
+  subtract(e) {
+    e.preventDefault();
+    const num1 = this.state.num1;
+    const num2 = this.state.num2;
+    const result = ((num1 === "" && num2 === "") ? 0 : num1 - num2);
+    this.setState({result});
+  }
+
+  multiply(e) {
+    e.preventDefault();
+    const num1 = this.state.num1;
+    const num2 = this.state.num2;
+    const result = ((num1 === "" && num2 === "") ? 0 : num1 * num2);
+    this.setState({result});
+  }
+
+  divide(e) {
+    e.preventDefault();
+    const num1 = this.state.num1;
+    const num2 = this.state.num2;
+    const result = ((num1 === "" && num2 === "") ? 0 : num1 / num2);
+    this.setState({result});
+  }
+
+  clear(e) {
+    e.preventDefault();
+    this.setState({num1: "", num2: "", result: 0});
+  }
+
+  render(){
+    const { num1, num2, result } = this.state;
+    const { setNum1, setNum2, add, subtract, multiply, divide, clear } = this;
+    return (
+      <div>
+        <input type="text" onChange={setNum1} value={num1}></input>
+        <input type="text" onChange={setNum2} value={num2}></input>
+        <h1>{result}</h1>
+        <button onClick={add}>+</button>
+        <button onClick={subtract}>-</button>
+        <button onClick={multiply}>*</button>
+        <button onClick={divide}>/</button>
+        <button onClick={clear}>Clear</button>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
